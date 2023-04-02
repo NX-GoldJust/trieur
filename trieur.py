eab2ccd3ff17b71924f8cbc3afc6ca0cd6edc58d
NX-GoldJust <117308582+NX-GoldJust@users.noreply.github.com>
2023-04-02 12:23:55 +0200

    Ajout Format audio

--- trieur.py
+++ trieur.py
@@ -9,3 +9,6 @@ video = ["mkv", "mp4", "avi", "mov", "wmv"]
 
 
 doc = ["txt", "pdf", "odt", "doc", "docx", "xps", "rtf", "xml"]
+
+
+audio = ["mp3", "wav", "ogg", "wma", "mid", "acc", "flac", "alac", "aiff", "dsd"]
