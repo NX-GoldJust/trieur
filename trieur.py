cf438bcb1df421b9011bef9659b32c027c42630f
NX-GoldJust <117308582+NX-GoldJust@users.noreply.github.com>
2023-04-02 12:21:49 +0200

    Ajout document format

--- trieur.py
+++ trieur.py
@@ -6,3 +6,6 @@ import os, shutil
 img = ["png", "jpeg", "jpg", "webp", "bmp", "tif", "svg", "ico"]
 
 video = ["mkv", "mp4", "avi", "mov", "wmv"]
+
+
+doc = ["txt", "pdf", "odt", "doc", "docx", "xps", "rtf", "xml"]
